4441432167f5b8c9d67255e66a069f3077e76f0a
progyu <rocket7101@gmail.com>
2019-11-10 08:14:48 +0900

    Context tutorial

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import ColorBox from "./components/ColorBox";
+import { ColorProvider } from "./contexts/color";
+import SelectColors from "./components/SelectColors";
+
+function App() {
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+}
+
+export default App;
