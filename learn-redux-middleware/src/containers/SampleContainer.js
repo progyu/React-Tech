84cb228688440378d676613e7a6b85b2e8a62dbe
progyu <rocket7101@gmail.com>
2019-12-10 14:33:59 +0900

    refactor: Create loading redux module

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -6,37 +6,44 @@ import { getPost, getUsers } from '../modules/sample';
 const { useEffect } = React;
 
 const SampleContainer = ({
-  getPost,
-  getUsers,
-  post,
-  users,
-  loadingPost,
-  loadingUsers
+	getPost,
+	getUsers,
+	post,
+	users,
+	loadingPost,
+	loadingUsers,
 }) => {
-  // 클래스 형태 컴포넌트에서는 componentDidMount
-  useEffect(()=> {
-    getPost(1);
-    getUsers(1);
-  }, [getPost, getUsers]);
-  return (
-    <Sample
-      post={post}
-      users={users}
-      loadingPost={loadingPost}
-      loadingUsers={loadingUsers}
-    />
-  )
-}
+	// 클래스 형태 컴포넌트에서는 componentDidMount
+	useEffect(() => {
+		const fn = async () => {
+			try {
+				await getPost(1);
+				await getUsers(1);
+			} catch (e) {
+				console.log(e); // 에러 조회
+			}
+		};
+		fn();
+	}, [getPost, getUsers]);
+	return (
+		<Sample
+			post={post}
+			users={users}
+			loadingPost={loadingPost}
+			loadingUsers={loadingUsers}
+		/>
+	);
+};
 
 export default connect(
-  ({ sample }) => ({
-    post: sample.post,
-    users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS
-  }),
-  {
-    getPost,
-    getUsers
-  }
+	({ sample, loading }) => ({
+		post: sample.post,
+		users: sample.users,
+		loadingPost: loading.GET_POST,
+		loadingUsers: loading.GET_USERS,
+	}),
+	{
+		getPost,
+		getUsers,
+	},
 )(SampleContainer);
