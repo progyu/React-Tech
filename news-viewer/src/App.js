48e8579a22150cbb65d8fdb69f28e61404893eac
progyu <rocket7101@gmail.com>
2019-12-11 19:07:34 +0900

    refactor: Get news api

---
+++ news-viewer/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=198704bb21044702b26ab96578b42619',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
