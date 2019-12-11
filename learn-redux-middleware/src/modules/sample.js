98ca5792bcf3b37e1eb6c9687413850efea3d6d0
progyu <rocket7101@gmail.com>
2019-12-11 16:10:54 +0900

    refactor: Using createRequestSage utill function

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,67 +1,27 @@
 import { createAction, handleActions } from 'redux-actions';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import { startLoading, finishLoading } from './loading';
+import createRequestSaga from '../lib/createRequestSaga';
 
 // 액션 타입 선업
 // 한 요청당 세 개를 만든다.
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = createAction(GET_POST, id => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-	yield put(startLoading(GET_POST)); // 로딩 시작
-	// 파라미터로 action을 받아오면 액션의 정보를 조회할 수 있습니다. 
-	try {
-		// call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있습니다.
-		// 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수 입니다.
-		const post = yield call(api.getPost, action.payload); // api.getPost(action.pay-load)를 의미
-		yield put({
-			type: GET_POST_SUCCESS,
-			payload: post.data
-		});
-	} catch (e) {
-		// try/catch 문을 사용하여 에러도 잡을 수 있습니다.
-		yield put({
-			type: GET_POST_FAILURE,
-			payload: e,
-			error: true
-		});
-	}
-	yield put(finishLoading(GET_POST)); // 로딩 완료
-}
-
-function* getUsersSaga(action) {
-	yield put(startLoading(GET_USERS)); 
-	try {
-		const users = yield call(api.getUsers); 
-		yield put({
-			type: GET_USERS_SUCCESS,
-			payload: users.data
-		});
-	} catch (e) {
-		yield put({
-			type: GET_USERS_FAILURE,
-			payload: e,
-			error: true
-		});
-	}
-	yield put(finishLoading(GET_USERS));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* sampleSaga() {
 	yield takeLatest(GET_POST, getPostSaga);
 	yield takeLatest(GET_USERS, getUsersSaga);
 }
-
 
 const initialState = {
 	post: null,
