caaf7b6ebb0736315ffe6295f270fd8e0347ce88
progyu <rocket7101@gmail.com>
2019-12-11 22:10:45 +0900

    feat: Add Fake news data
    Styiling using styled-components

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,34 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=198704bb21044702b26ab96578b42619',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
