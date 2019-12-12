41ff95aa9a1b49977ab4d8f4bfad55df5b2af075
progyu <rocket7101@gmail.com>
2019-12-12 19:22:38 +0900

    refactor: Split code without React.lazy, Suspense

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  const onClick = () => {
-    import ('./notify').then(result => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null
   };
-
-  return (
+  handleClick = async () => {
+    const loadModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadModule.default
+    })
+  }
+  render() {
+    const { SplitMe } = this.state;
+    return (
     <div className="App">
       <header className="App-header">
-        <p onClick={onClick}>Hello React!</p>
+        <p onClick={this.handleClick}>Hello React!</p>
+        {SplitMe && <SplitMe />}
       </header>
     </div>
-  );
+    )
+  }
 }
+
+
+// function App() {
+//   const onClick = () => {
+//     import ('./notify').then(result => result.default());
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p onClick={onClick}>Hello React!</p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
